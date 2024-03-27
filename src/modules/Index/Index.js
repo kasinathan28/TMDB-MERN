3a048071887fd145ab4501ecd97a86964b3157fb
kasinathan28 <way2kasinathb35@gmail.com>
2024-03-27 19:44:31 +0530

    TMDB-MERN

---
+++ src/modules/Index/Index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Index.css"
+import Navbar from '../../components/Navbar/Navbar'
+
+function Index() {
+  return (
+    <div className='index'>
+        <Navbar/>
+        <div className='main'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Index
