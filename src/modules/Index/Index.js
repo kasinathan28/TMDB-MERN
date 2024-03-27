5b6247c4acc4d8a3bd50fc8fa436c509d30caf27
kasinathan28 <way2kasinathb35@gmail.com>
2024-03-27 22:10:54 +0530

    Banner Completed

--- src/modules/Index/Index.js
+++ src/modules/Index/Index.js
@@ -2,12 +2,26 @@ import React from 'react'
 import "./Index.css"
 import Navbar from '../../components/Navbar/Navbar'
 
+import wave from "../../assets/wave.svg";
+import bg from "../../assets/bg.png";
+
 function Index() {
   return (
     <div className='index'>
         <Navbar/>
         <div className='main'>
-
+          <img src={bg} alt='bg' className='bgimg'/>
+          <div className='message'>
+          <h1>Welcome</h1>
+          <h2>Millions of Movies , TV Shows and People To Discover.</h2>
+          </div>
+          <div className='search'>
+            <input type='text' id='search'/>
+            <button>Search</button>
+          </div>
+          <div>
+          <img src={wave} alt='wave'  className='wave'/>
+          </div>
         </div>
     </div>
   )
