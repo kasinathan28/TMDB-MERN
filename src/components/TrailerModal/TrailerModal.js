f478be4266bb3908c0b06f9a3ea59d4f3829269c
kasinathan28 <way2kasinathb35@gmail.com>
2024-03-31 00:49:53 +0530

    Trailer Completed

--- src/components/TrailerModal/TrailerModal.js
+++ src/components/TrailerModal/TrailerModal.js
@@ -1,25 +1,50 @@
-// TrailerModal.js
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 import "./TrailerModal.css";
 
 function TrailerModal({ trailer, onClose }) {
+  const [loading, setLoading] = useState(true);
+  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth < 768);
+
+  const handleResize = () => {
+    setIsSmallScreen(window.innerWidth < 768);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const handleReady = () => {
+    setLoading(false);
+  };
+
+  const handleError = () => {
+    setLoading(false);
+  };
+
   return (
     <div className="modal">
       <button className="close-btn" onClick={onClose}>
-        Close
+        X
       </button>
-      <div className="modal-content">
-        {trailer && (
-          <ReactPlayer
-            url={trailer}
-            controls
-            wrapper={(props) => <div {...props} className="react-player" />} 
-            height={500}
-            width={900}
-          />
-        )}
-      </div>
+      {loading && (
+        <div className="loader-container1">
+          <div className="loader1"></div>
+        </div>
+      )}
+      {trailer && (
+        <ReactPlayer
+          url={trailer}
+          controls
+          width={isSmallScreen ? 400 : 1100}
+          height={isSmallScreen ? 200 : 600}
+          onReady={handleReady}
+          onError={handleError}
+        />
+      )}
     </div>
   );
 }
