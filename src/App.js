70a833973710458174cb3ede5b5d7d7336caee4e
kasinathan28 <way2kasinathb35@gmail.com>
2024-03-31 23:21:03 +0530

    Added lazzy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
+import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Index from './modules/Index/Index';
-import Dashboard from './modules/dash/Dashboard';
-import Auth from './modules/auth/Auth';
-import Signup from './modules/Signup/Signup';
-import Search from './modules/Search/Search';
-import Details from './modules/Details/Details';
+
+const Index = React.lazy(() => import('./modules/Index/Index'));
+const Dashboard = React.lazy(() => import('./modules/dash/Dashboard'));
+const Auth = React.lazy(() => import('./modules/auth/Auth'));
+const Signup = React.lazy(() => import('./modules/Signup/Signup'));
+const Search = React.lazy(() => import('./modules/Search/Search'));
+const Details = React.lazy(() => import('./modules/Details/Details'));
 
 function App() {
   return (
     <div>
       <Router>
-        <Routes>
-          <Route path = "/" element={<Index/>}/>
-          <Route path = "/join" element={<Signup/>}/>
-          <Route path='/auth' element={<Auth/>}/>
-          <Route path = "/dash" element={<Dashboard/>}/>
-          <Route path="/search/:query" element={<Search />} />
-          <Route path='/details/:mediaType/:id' element={<Details/>}/>
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/join" element={<Signup />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/dash" element={<Dashboard />} />
+            <Route path="/search/:query" element={<Search />} />
+            <Route path="/details/:mediaType/:id" element={<Details />} />
+          </Routes>
+        </Suspense>
       </Router>
     </div>
   );
